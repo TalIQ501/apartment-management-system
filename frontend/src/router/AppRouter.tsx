4dbdedc6af8d438690a13960c7c3dffdf5909981
TalIQ501 <talhaiqbal501@gmail.com>
2025-11-02 19:49:34 +0500

    Initial Frontend

---
+++ frontend/src/router/AppRouter.tsx
@@ -0,0 +1,21 @@
+import { Suspense } from "react";
+import { Loading } from "../components/Loading";
+import { useLocation, useRoutes } from "react-router-dom";
+import { routes } from "./AppRoutes";
+import { Navbar } from "../components/Navbar/Navbar";
+
+export const AppRouter = () => {
+  const location = useLocation();
+  const element = useRoutes(routes, location)
+
+  return (
+    <>
+    <Navbar />
+    <section className="flex flex-col gap-4 w-full max-h-10">
+      <Suspense fallback={<Loading />}>
+        {element}
+      </Suspense>
+    </section>
+    </>
+  );
+};
---
+++ frontend/src/router/AppRoutes.tsx
@@ -0,0 +1,10 @@
+import { lazy } from "react";
+import type { RouteObject } from "react-router-dom";
+
+const HomePage = lazy(() => (
+    import("../pages/HomePage").then((module) => ({ default : module.HomePage }))
+))
+
+export const routes : RouteObject[] = [
+    { path: "/", element: <HomePage /> },
+]
